514fa97f8ef74cf9fa0b5bdb9a5930f4c959cb21
vilesovds <vilesovds@gmail.com>
2020-11-08 16:25:06 +0300

    Solutions for parst 1

---
+++ hw3/task1.py
@@ -0,0 +1,39 @@
+# simple way
+
+def simple_div(a, b):
+    """
+    division procedure
+    :param a: dividend number
+    :param b: divisor number
+    :return: division result or None
+    """
+    if b != 0:
+        return a/b
+
+
+# decorator way
+
+def anti_zero_div_exception(fn):
+    def core_func(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except ZeroDivisionError as err:
+            pass
+    return core_func
+
+
+@anti_zero_div_exception
+def vars_division(a, b):
+    """
+    simple division a/b
+    :param a: dividend number
+    :param b: divisor number
+    :return: division result
+    """
+    return a/b
+
+
+number1 = float(input('Please inter number a: '))
+number2 = float(input('Please inter number b: '))
+print('Result simple_div   : ', simple_div(number1, number2))
+print('Result vars_division: ', vars_division(number1, number2))
