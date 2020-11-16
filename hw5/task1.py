0c56c9dc030f76d5f2df81280fb1d6d81c682ec6
vilesovds <vilesovds@gmail.com>
2020-11-16 22:40:16 +0300

    Refactoring task1.py. Tests was separated and refactored to parametrize

--- hw5/task1.py
+++ hw5/task1.py
@@ -4,45 +4,15 @@
 Об окончании ввода данных свидетельствует пустая строка.
 """
 
-FILENAME = 'task1_tmp.txt'
 
-
-def main():
-    with open(FILENAME, 'w') as f:
+def collaborate(file_path):
+    with open(file_path, 'w') as f:
         while True:
-            i = input("Please enter some string or type Enter to exit")
+            i = input("Please enter some string or type Enter to exit: ")
             if i == '':
                 break
             print(i, file=f)
 
 
 if __name__ == '__main__':
-    main()
-
-
-def test_main_empty(monkeypatch):
-    import os.path
-    # simple empty mock
-    monkeypatch.setattr('builtins.input', lambda _: '')
-    main()
-    assert os.path.isfile(FILENAME)
-    assert os.path.getsize(FILENAME) == 0
-
-
-def test_main_list(monkeypatch):
-    import os.path
-    test_content = ["Mark", "Antony", "Lisa"]
-    # prepare test data
-    gen_list = test_content.copy()
-    gen_list.append('')
-    gen = (x for x in gen_list)
-
-    # list mock
-    monkeypatch.setattr('builtins.input', lambda _: next(gen))
-
-    main()
-    assert os.path.isfile(FILENAME)
-    with open(FILENAME, 'r') as f:
-        # we should remove last \n symbol before compare
-        assert [rl[:-1] for rl in f.readlines()] == test_content
-    os.remove(FILENAME)
+    collaborate('task1_tmp.txt')
---
+++ hw5/tests/test_task1.py
@@ -0,0 +1,24 @@
+import pytest
+from task1 import collaborate
+
+
+@pytest.mark.parametrize("input_list, expected", [(["Mark", "Antony", "Lisa"], 'Mark\nAntony\nLisa\n'),
+                                                  (["Mark"], 'Mark\n'),
+                                                  ([''], '')])
+def test_content_list(monkeypatch, input_list, expected):
+    import os.path
+    test_file = '1.txt'
+    gen_list = input_list.copy()
+    # to stop
+    gen_list.append('')
+    gen = (x for x in gen_list)
+
+    # list mock
+    monkeypatch.setattr('builtins.input', lambda _: next(gen))
+
+    collaborate(test_file)
+
+    assert os.path.isfile(test_file)
+    with open(test_file, 'r') as f:
+        assert f.read() == expected
+    os.remove(test_file)
