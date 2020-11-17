4cb2307b42575f7f58e09e3bf70c5d1580ba02bf
Вилесов Дмитрий Сергеевич <d.vilesov@owen.ru>
2020-11-17 11:31:51 +0300

    refactoring task6 - removed tests fixed task6 don't analyze last symbol in line refactoring test_task6.py:     using parametrize     separated tests for good content and ugly     added test_non_exists_file with mock

--- hw5/task6.py
+++ hw5/task6.py
@@ -26,7 +26,7 @@ def calculate_hours(file_path):
     try:
         with open(file_path, 'r') as f:
             for line in f:
-                subject, numbers = line.split(':')
+                subject, numbers = line[:-1].split(':')
                 subject_sum = sum(int(x.split('(')[0]) for x in numbers.split(' ') if len(x) and x != '—' and x != '-')
                 result[subject] = subject_sum
     except Exception as err:
@@ -38,29 +38,3 @@ if __name__ == '__main__':
     from pprint import pprint
     res_dict = calculate_hours('task6_test.txt')
     pprint(res_dict, width=1)
-
-
-def test_positive():
-    import os
-    test_file = '1.txt'
-    test_content = "Информатика: 100(л) 50(пр) 20(лаб)\n" \
-                   "Физика: 30(л) - 10(лаб)\n" \
-                   "Физкультура: - 30(пр) -"
-    with open(test_file, 'w') as f:
-        f.write(test_content)
-
-    assert calculate_hours(test_file) == {"Информатика": 170, "Физика": 40, "Физкультура": 30}
-    os.remove(test_file)
-
-
-def test_non_exists_file():
-    assert {} == calculate_hours('1')
-
-
-def test_empty_file():
-    import os
-    f_path = 'tmp'
-    f = open(f_path, 'w')
-    f.close()
-    assert {} == calculate_hours(f_path)
-    os.remove(f_path)
