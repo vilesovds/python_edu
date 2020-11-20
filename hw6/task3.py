a857cb8c690ab9441176986be3ec0a3f78c76289
Вилесов Дмитрий Сергеевич <d.vilesov@owen.ru>
2020-11-20 17:16:40 +0300

    Added few comments

--- hw6/task3.py
+++ hw6/task3.py
@@ -20,6 +20,13 @@ class Worker:
     _income = {'wage': None, 'bonus': None}
 
     def __init__(self, name, surname, position, wage, bonus):
+        """
+        :param name: str
+        :param surname: str
+        :param position: str
+        :param wage: wage
+        :param bonus: bonus
+        """
         self.position = position
         self.name = name
         self.surname = surname
@@ -28,10 +35,21 @@ class Worker:
 
 
 class Position(Worker):
+    """
+    Position storage
+    """
     def get_full_name(self):
+        """
+        Name plus surname
+        return: str
+        """
         return f'{self.name} {self.surname}'
 
     def get_total_income(self):
+        """
+        Sum of wage and bonus
+        return: number
+        """
         return sum(self._income.values())
 
 
