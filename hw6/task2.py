a5e45aa78ce3e73a36e2a7b65cd5c27301b4034e
Вилесов Дмитрий Сергеевич <d.vilesov@owen.ru>
2020-11-20 11:47:17 +0300

    added task2 solution

---
+++ hw6/task2.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Реализовать класс Road (дорога), в котором определить атрибуты: length (длина), width (ширина).
+Значения данных атрибутов должны передаваться при создании экземпляра класса.
+Атрибуты сделать защищенными.
+Определить метод расчета массы асфальта, необходимого для покрытия всего дорожного полотна.
+Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв метра дороги асфальтом,
+толщиной в 1 см * число см толщины полотна. Проверить работу метода.
+"""
+
+
+class Road:
+    """
+    Storage length and width of real road.
+    """
+    __length = 0
+    __width = 0
+
+    def __init__(self, length, width):
+        """
+        :param length: number
+        :param width: number
+        """
+        self.__length = length
+        self.__width = width
+
+    def calc_mass(self, density, thickness):
+        """
+        Mass calculation as w*l*h*rho
+        :param density: number
+        :param thickness: number
+        :return: number, kg
+        """
+        return self.__width * self.__length * density * thickness
+
+
+if __name__ == '__main__':
+    road = Road(500, 10)
+    print(f'total mass of asphalt {road.calc_mass(9.35, 5.5)} kg')
