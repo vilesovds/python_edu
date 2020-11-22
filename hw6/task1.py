2e69aab2b4388f8316fda7f7e386bc5f2daedcdf
vilesovds <vilesovds@gmail.com>
2020-11-22 17:08:50 +0300

    added additional values check in applay_times

--- hw6/task1.py
+++ hw6/task1.py
@@ -8,12 +8,11 @@ class TrafficLight:
     __default_timers = {'red': 7, 'yellow': 2, 'green': 5}
 
     def __init__(self, **kwargs):
-        """"""
         self.__times = TrafficLight.__default_timers.copy()
         self.apply_times(**kwargs)
         self.__color = cycle(TrafficLight.__colors)
 
-    def get_colors(self):
+    def get_colors():
         return TrafficLight.__colors
 
     def set_default_timers(self):
@@ -21,10 +20,15 @@ class TrafficLight:
 
     def apply_times(self, **kwargs):
         for k, v in kwargs.items():
-            if k in self.__times.keys():
-                if v <= 0:
-                    raise ValueError("Unsupported value for time")
-                self.__times[k] = v
+            try:
+                v = int(v)
+            except TypeError:
+                raise TypeError("Unexpected value type. Need integer")
+            if v <= 0:
+                raise ValueError("Unsupported value for time")
+            if k not in self.__times.keys():
+                raise KeyError(f"Params should be one of this: {', '.join(self.__times.keys())}")
+            self.__times[k] = v
 
     def running(self):
         for light in self.__color:
