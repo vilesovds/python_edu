e27baac0e15fbe0bd77d31e532ec782e75473f69
Вилесов Дмитрий Сергеевич <d.vilesov@owen.ru>
2020-11-24 18:51:05 +0300

    added few tests for task3.py

---
+++ hw6/tests/test_task3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pytest
+from task3 import Worker, Position
+
+
+@pytest.mark.parametrize("name, surname, position, wage, bonus, expected", [
+    # usual
+    ('Ivan', 'Petrov', 'Teacher', 15550, 520, ('Ivan', 'Petrov', 'Teacher', {'wage': 15550, 'bonus': 520})),
+    # one
+    ('', '', '', 0, 0, ('', '', '', {'wage': 0, 'bonus': 0}))])
+def test_create(name, surname, position, wage, bonus, expected):
+    p = Position(name, surname, position, wage, bonus)
+    assert expected == (p.name, p.surname, p.position, p._income)
+
+
+@pytest.mark.parametrize("wage, bonus, expected", [
+    # usual
+    (15550, 520, 16070),
+    # zero
+    (0, 0, 0)])
+def test_total_income(wage, bonus, expected):
+    p = Position('A', 'B', 'C', wage, bonus)
+    assert expected == p.get_total_income()
