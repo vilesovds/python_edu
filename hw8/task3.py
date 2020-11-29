b75916ad5efe0cc85a2e91d2d13ede3d2e62d952
Вилесов Дмитрий Сергеевич <d.vilesov@owen.ru>
2020-11-30 00:27:09 +0300

    working version of task3

--- hw8/task3.py
+++ hw8/task3.py
@@ -18,4 +18,50 @@ class NumbersOnly(Exception):
         return self.txt
 
 
-raise NumbersOnly("не знаю что написать", 12, [11, 10, 9], 'Ошибка')
+class CheckedList:
+    _exception = NumbersOnly('only numbers are supported')
+    __list = []
+    # int should be the first
+    supported_types = (int, float)
+
+    def append(self, data):
+        """
+        Added one element
+        :param data: number or string representation of a number
+        :return: None
+        """
+        t = type(data)
+        if t in self.supported_types:
+            self.__list.append(data)
+            return
+        elif t == str:
+            for f in self.supported_types:
+                try:
+                    num = f(data)
+                except ValueError:
+                    continue
+                else:
+                    self.__list.append(num)
+                    return
+
+        raise self._exception
+
+    @property
+    def numbers_list(self):
+        return self.__list
+
+    def __str__(self):
+        return str(self.numbers_list).strip('[]')
+
+
+if __name__ == '__main__':
+    cl = CheckedList()
+    while True:
+        ui = input("Please input number: ")
+        if not ui:
+            break
+        try:
+            cl.append(ui)
+        except NumbersOnly as err:
+            print(f'Input error: {err}')
+    print(f'Result: {cl}')
